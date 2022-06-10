ec7157b464e64d7c78c181cc87c85753f32c872c
lennartfu <lennartfu@outlook.com>
2022-06-10 20:25:13 +0200

    Aufgabe 8

--- src/cgg/Image.java
+++ src/cgg/Image.java
@@ -1,101 +1,91 @@
-/** @author henrik.tramberend@beuth-hochschule.de */
+/**
+ * @author henrik.tramberend@beuth-hochschule.de
+ */
 package cgg;
 
+import cgg.a09.OnePixel;
+import cgg.a09.Pixel;
 import cgtools.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Image {
 
-  private final int samplePoints = 100;
+    public static final int samplePoints = 1;
 
-  private final int components = 3;
-  private int width;
-  private int height;
-  private double[] data;
+    private final int components = 3;
+    private int width;
+    private int height;
+    private double[] data;
 
-  public Image(int width, int height) {
-    this.width = width;
-    this.height = height;
-    this.data = new double[width * height * components];
-  }
+    public Image(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.data = new double[width * height * components];
+    }
 
-  public void setPixel(int x, int y, Color color) {
-    int indexRed = components * (y * width + x);
-    int indexGreen = indexRed + 1;
-    int indexBlue = indexRed + 2;
+    public void setPixel(int x, int y, Color color) {
+        int indexRed = components * (y * width + x);
+        int indexGreen = indexRed + 1;
+        int indexBlue = indexRed + 2;
 
-    color = correctGamma(color);
+        color = correctGamma(color);
 
-    data[indexRed] = color.r();
-    data[indexGreen] = color.g();
-    data[indexBlue] = color.b();
-  }
+        data[indexRed] = color.r();
+        data[indexGreen] = color.g();
+        data[indexBlue] = color.b();
+    }
 
-  private Color correctGamma(Color color) {
-    final double gamma = 2.2;
-    double r = Math.pow(color.r(), 1 / gamma);
-    double g = Math.pow(color.g(), 1 / gamma);
-    double b = Math.pow(color.b(), 1 / gamma);
-    return new Color(r, g, b);
-  }
+    private Color correctGamma(Color color) {
+        final double gamma = 2.2;
+        double r = Math.pow(color.r(), 1 / gamma);
+        double g = Math.pow(color.g(), 1 / gamma);
+        double b = Math.pow(color.b(), 1 / gamma);
+        return new Color(r, g, b);
+    }
 
-  public void write(String filename) {
-    ImageWriter.write(filename, data, width, height);
-  }
+    public void write(String filename) {
+        ImageWriter.write(filename, data, width, height);
+    }
 
-  public void sample(Sampler s, boolean useStratifiedSampling) {
-    int i = 0;
-    // Iterates over all pixel positions inside this image.
-    for (int x = 0; x != width; x++) {
-      for (int y = 0; y != height; y++) {
-        Color color;
-        if (useStratifiedSampling) {
-          // Supersampling
-          color = superSample(s, x, y);
-        } else {
-          color = s.getColor(x, y);
+    public void sample(Sampler s, boolean useStratifiedSampling) {
+        int threads = Runtime.getRuntime().availableProcessors();
+        ExecutorService pool = Executors.newFixedThreadPool(threads);
+        List<Future<Pixel>> pixels = new ArrayList<>();
+        // Iterates over all pixel positions inside this image.
+        for (int x = 0; x != width; x++) {
+            for (int y = 0; y != height; y++) {
+                pixels.add(pool.submit(new OnePixel(s, x, y, useStratifiedSampling)));
+            }
         }
-        // Sets the color for one particular pixel.
-        this.setPixel(x, y, color);
-      }
-      if (x % (width / 20) == 0) {
-        System.out.print(i + "%... ");
-        i += 5;
-      }
+        int i = 0;
+        for (Future<Pixel> future : pixels) {
+            try {
+                Pixel pixel = future.get();
+                // Sets the color for one particular pixel.
+                this.setPixel(pixel.x(), pixel.y(), pixel.color());
+                if (pixel.x() % (width / 20) == 0 && pixel.y() == 0) {
+                    System.out.print(i + "%... ");
+                    i += 5;
+                }
+            } catch (InterruptedException | ExecutionException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
 
-  private Color superSample(Sampler s, int x, int y) {
-    List<Color> pixelColors = new ArrayList<>();
-    // Get the color from a random point in the pixel 100 times and add it to a list.
-    for (int i = 0; i < samplePoints; i++) {
-      double scanX = x + Random.random();
-      double scanY = y + Random.random();
-      pixelColors.add(s.getColor(scanX, scanY));
+    private void notYetImplemented() {
+        System.err.println("Please complete the implementation of class cgg.Image as part of assignment 1.");
+        System.exit(1);
     }
-    // Calculate the average from all colors in the list.
-    Color color = pixelColors.remove(0);
-    for (Color c: pixelColors) {
-      double r = color.r() + c.r();
-      double g = color.g() + c.g();
-      double b = color.b() + c.b();
-      color = new Color(r, g, b);
+
+    public static String getFilepath(String filename) {
+        String projectDir = System.getProperty("user.dir");
+        return projectDir + "/doc/" + filename;
     }
-    double r = color.r() / samplePoints;
-    double g = color.g() / samplePoints;
-    double b = color.b() / samplePoints;
-    return new Color(r, g, b);
-  }
-
-  private void notYetImplemented() {
-    System.err.println("Please complete the implementation of class cgg.Image as part of assignment 1.");
-    System.exit(1);
-  }
-
-  public static String getFilepath(String filename) {
-    String projectDir = System.getProperty("user.dir");
-    return projectDir + "/doc/" + filename;
-  }
 }
